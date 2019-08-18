b600173b23cd637e1d2485edd32b40281d62140b
Piyush2769 <startcheckingyourmail@gmail.com>
2019-08-18 16:13:36 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(new BlogApp());
+}
+
+class BlogApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Blog App",
+
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+
+}
